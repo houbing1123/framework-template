c67d7b8601a189a78e790f6bb07800fd54c05ae8
houbing1123 <1604063822@qq.com>
2025-03-19 22:06:01 +0800

    测试提交

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import router from './router/routes';
 import { RouterProvider } from 'react-router-dom';
 
 const App: React.FC = () => {
-  console.log('📌 动态路由数据');
-  console.dir(router);
+
   
   return (
     <RouterProvider router={router} />
